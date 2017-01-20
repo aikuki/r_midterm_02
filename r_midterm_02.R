821e09a6c570b34112765f8587428f55f088a47e
aikuki <ohasiyukie@gmail.com>
2017-01-20 23:38:46 +0800

    first commit

---
+++ r_midterm_02.R
@@ -0,0 +1,14 @@
+#define my sd()
+my_sd <- function(input_vec) {
+  length_input_vec <- length(input_vec)   
+  n_minus_one <- length_input_vec - 1 #n-1
+  x_bar <- mean(input_vec) #•½‹ÏÉ
+  summation <- 0
+  for(i in input_vec) {
+    summation <- summation + (i - x_bar)^2
+    ans_end <- summation / n_minus_one
+  }
+  return(sqrt(ans_end))
+}
+
+my_sd(c(1, 2, 3, 4))
